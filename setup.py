0dd510921184191bcdfaef4a6db88bb3d3ca2ca4
SThiwanka <thiwankachandithasinhalage@gmail.com>
2026-02-08 02:38:28 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MediChat",
+    version="0.1.0",
+    author="Thiwanka Chanditha",
+    author_email="thiwankachandithasinhalage@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
